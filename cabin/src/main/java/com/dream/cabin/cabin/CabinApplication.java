2f99941d6fa6d2dc7694008d857c33496f759e00
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-05 16:45:47 -0800

    feat: Initial project commit.

---
+++ cabin/src/main/java/com/dream/cabin/cabin/CabinApplication.java
@@ -0,0 +1,13 @@
+package com.dream.cabin.cabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinApplication.class, args);
+	}
+
+}
---
+++ cabin/src/test/java/com/dream/cabin/cabin/CabinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.cabin.cabin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
