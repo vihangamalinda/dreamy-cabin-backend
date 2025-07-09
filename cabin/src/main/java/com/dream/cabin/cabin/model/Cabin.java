fe30fb0bfe659da4fceb4d6d9ae1566eb973e740
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-09 02:34:05 -0800

    feat:Cabin entity.

--- cabin/src/main/java/com/dream/cabin/cabin/model/Cabin.java
+++ cabin/src/main/java/com/dream/cabin/cabin/model/Cabin.java
@@ -7,88 +7,100 @@ import java.util.Date;
 @Entity
 public class Cabin {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
-    private Integer id;
-    @Column(name="name")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
     private String name;
 
-    @Column(name="image_url")
+    @Column(name = "image_url")
     private String imageUrl;
-    @Column(name="description")
+    @Column(name = "description")
     private String description;
     @Column(name = "max_capacity")
     private int maxCapacity;
-    @Column(name="discount")
+    @Column(name = "discount")
     private int discount;
     @Column(name = "is_active")
     private boolean isActive;
 
 
-    @Column(name="created_at")
+    @Column(name = "created_at")
     final private Date createdAt;
 
-    public Cabin(){
-        this.createdAt =new Date();
-    }
-    public Cabin(int id,String name,String location,String detail){
-        this.id =id;
-        this.name=name;
-        this.imageUrl=location;
-        this.description =detail;
+    public Cabin() {
         this.createdAt = new Date();
     }
 
-    public int getDiscount(){
-        return this.discount;
-    }
-    public void setDiscount(int discount){
-        this.discount =discount;
-    }
-    public int getMaxCapacity(){
-        return  this.maxCapacity;
-    }
-    public void setMaxCapacity(int maxCapacity){
-        this.maxCapacity =maxCapacity;
-    }
-    public boolean getIsActive(){
-        return  this.isActive;
+    public Cabin(int id, String name, String location, String detail) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = location;
+        this.description = detail;
+        this.createdAt = new Date();
     }
 
-    public void setIsActive(boolean isActive){
-        this.isActive =isActive;
+    public int getDiscount() {
+        return this.discount;
     }
-    public int getId(){
-        return  this.id;
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
     }
-    public void setId(int id){
+
+    public int getMaxCapacity() {
+        return this.maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public boolean getIsActive() {
+        return this.isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
         this.id = id;
     }
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
-    public void setName(String name){
-        this.name =name;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public String getImageUrl(){
+
+    public String getImageUrl() {
         return this.imageUrl;
     }
-    public void setImageUrl(String imageUrl){
-        this.imageUrl =imageUrl;
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return this.description;
     }
 
-    public void setDescription(String description){
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public Date getCreatedAt(){
-        return  this.createdAt;
+    public Date getCreatedAt() {
+        return this.createdAt;
     }
 
     @Override
-    public String toString(){
-        return "id :"+this.id+" name: "+ this.name;
+    public String toString() {
+        return "id :" + this.id + " name: " + this.name;
     }
 }
