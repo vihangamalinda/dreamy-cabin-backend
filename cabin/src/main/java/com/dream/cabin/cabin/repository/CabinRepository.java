507dc6fa359e1242be67ddaa646d10e629839b2c
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-09 02:35:24 -0800

    feat:adding custom JPA method to retrieve is active data.

--- cabin/src/main/java/com/dream/cabin/cabin/repository/CabinRepository.java
+++ cabin/src/main/java/com/dream/cabin/cabin/repository/CabinRepository.java
@@ -4,9 +4,9 @@ import com.dream.cabin.cabin.model.Cabin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CabinRepository extends JpaRepository<Cabin,Integer> {
 import java.util.List;
 
 @Repository
 public interface CabinRepository extends JpaRepository<Cabin, Long> {
+   List<Cabin> findByIsActiveTrue();
 }
