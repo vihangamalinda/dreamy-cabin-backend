8291973f2ed244dabf0eb17fa1f7baf28f31571b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-09 02:34:46 -0800

    feat:adding cabin related JPARepository.

--- cabin/src/main/java/com/dream/cabin/cabin/repository/CabinRepository.java
+++ cabin/src/main/java/com/dream/cabin/cabin/repository/CabinRepository.java
@@ -2,6 +2,11 @@ package com.dream.cabin.cabin.repository;
 
 import com.dream.cabin.cabin.model.Cabin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface CabinRepository extends JpaRepository<Cabin,Integer> {
+import java.util.List;
+
+@Repository
+public interface CabinRepository extends JpaRepository<Cabin, Long> {
 }
