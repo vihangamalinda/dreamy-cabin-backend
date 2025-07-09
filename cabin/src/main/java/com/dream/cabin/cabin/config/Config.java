937a09babb2abce78413ce9e2f9c80ae36b4aa82
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2025-07-09 02:37:54 -0800

    feat:adding a configuration file for manual bean configurations.

---
+++ cabin/src/main/java/com/dream/cabin/cabin/config/Config.java
@@ -0,0 +1,24 @@
+package com.dream.cabin.cabin.config;
+
+import com.dream.cabin.cabin.repository.CabinRepository;
+import com.dream.cabin.cabin.service.CabinService;
+import com.dream.cabin.cabin.service.impl.CabinServiceImpl;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class Config {
+
+}
+
